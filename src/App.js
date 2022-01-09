61089da442f6e51c8cacaf1fe2852c41a9b23c3f
MichaelR <spider747@gmail.com>
2022-01-09 21:06:37 +0200

    Add navbar, define routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Navbar, Sidebar, Footer } from './components';
+
+import {
+  Home,
+  SingleProduct,
+  Cart,
+  Checkout,
+  Error,
+  About,
+  Products,
+  PrivateRoute,
+} from './pages';
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+    <Navbar/>
+    <Sidebar/>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/about" component={About}/>
+        <Route exact path="/cart" component={Cart}/>
+        <Route exact path="/products" component={Products}/>
+        <Route exact path="/products/:id" children={SingleProduct}/>
+        <Route exact path="/chekout" component={Checkout}/>
+        <Route path="*" component={Error}/>
+      </Switch>
+      <Footer/>
+    </Router>
+  );
 }
 
-export default App
+export default App;
